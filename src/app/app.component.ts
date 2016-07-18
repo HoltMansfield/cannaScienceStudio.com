191c00248bd13cab57699e020e8756f0c6477a42
Holt Mansfield <holt.mansfield@mobilelive.ca>
2016-07-18 13:23:11 -0400

    adding ng-bootstrap and sass from boostrap 4.x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import {NGB_DIRECTIVES} from '@ng-bootstrap/ng-bootstrap';
 
 import { ApiService } from './shared';
 
 import '../style/app.scss';
+import '../style/test.scss';
+import '../style/bootstrap/scss/bootstrap.scss';
 
 
 @Component({
-  selector: 'my-app', 
+  selector: 'my-app',
   providers: [ApiService],
-  directives: [...ROUTER_DIRECTIVES],
+  directives: [
+    ...ROUTER_DIRECTIVES,
+    NGB_DIRECTIVES
+    ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
